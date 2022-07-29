5013e117dd21fbf30d820123841fb854e4b73e03
Amirxon Raximov <arm19980201@gmail.com>
2022-07-29 23:30:32 +0500

    save

--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -3,11 +3,13 @@ package lang.print.gaps.task2;
 public class CopiedNames {
     public static void main(String[] args) {
         int variable = 0;
+        int variable_one = 1000;
         int variableTwo = 2000;
-        int value = 1000;
-        int variableThree = 4000;
-        int value2 = 3000;
-        int variables = 7000;
-        int value3 = 90000;
+        int variable_three = 3000;
+        int VARIABLE_FOUR = 4000;
+        int VARIABLE_FIVE = 5000;
+        int variableSix = 6000;
+        int variableSeven = 7000;
+        int variableEight = 8000;
     }
 }
--- src/main/java/lang/print/gaps/task2/NamingConvention.java
+++ src/main/java/lang/print/gaps/task2/NamingConvention.java
@@ -2,14 +2,14 @@ package lang.print.gaps.task2;
 
 public class NamingConvention {
     public static void main(String[] args) {
-        int variable = 10;
-        int variableOne = 1;
-        int variableTwo = 2;
-        int variableThree = 3;
-        final int VARIABLE_FOUR = 4;
-        final int VARIABLE_FIVE = 5;
-        int variableSix = 6;
-        int variableSeven = 7;
-        int variableEight = 8;
+        int variable = 0;
+        int variable_one = 1000;
+        int variableTwo = 2000;
+        int variable_three = 3000;
+        int VARIABLE_FOUR = 4000;
+        int VARIABLE_FIVE = 5000;
+        int variableSix = 6000;
+        int variableSeven = 7000;
+        int variableEight = 8000;
     }
 }
